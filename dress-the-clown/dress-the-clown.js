ad60f7a9935137577b3ddd355330307899e88b87
Johnny <jvsainsbury@gmail.com>
2021-03-07 14:18:01 +1300

    Initial commit

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -3,3 +3,18 @@
 // -    -   -   -   -  //
 
 console.log("Dress The Clown!")
+
+var headStr1 = '"./images/head';
+var headIndex = 0;
+var headStr2 = '.png"'
+
+function changeClownHead() {
+  headIndex++;
+  var headSrc = headStr1.concat(headIndex, headStr2);
+  var headImg = document.getElementById("head");
+  headImg.src = headSrc;
+}
+
+changeClownHead;
+
+console.log(changeClownHead)
