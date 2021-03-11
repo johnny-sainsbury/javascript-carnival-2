135ef3d9a33e84becf1777255cc469a5015b71ac
Johnny <jvsainsbury@gmail.com>
2021-03-11 17:16:03 +1300

    Complete left arrow key function

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -4,17 +4,33 @@
 
 console.log("Dress The Clown!")
 
-var headStr1 = './images/head';
-var headIndex = 0;
-var headStr2 = '.png';
+const headStr1 = './images/head';
+let headIndex = 0;
+const headStr2 = '.png';
 
 function changeClownHead() {
-  headIndex++;
-  var headSrc = headStr1.concat(headIndex, headStr2);
-  var headImg = document.getElementById("head");
-  headImg.src = headSrc;
+  if (headIndex < 5) {
+    headIndex++;
+    const headImg = document.getElementById("head");
+    const headSrc = `${headStr1}${headIndex}${headStr2}`;
+    headImg.src = headSrc;
+  } else {
+    headIndex = 0;
+    }
 }
 
-changeClownHead();
+document.onkeydown = checkKey;
 
-console.log(changeClownHead)
+function checkKey(e) {
+  e = e || window.event;
+  if (e.keyCode == '38') {
+    // up arrow
+  } else if (e.keyCode == '40') {
+   // down arrow
+  } else if (e.keyCode == '37') {
+    // left arrow
+  } else if (e.keyCode == '39') {
+    // right arrow
+    changeClownHead();
+  }
+}
